f2dc1c0bc4b50f39de4056552820922d9bc22763
Tatsuka <[tatsuka.m18@gmail.com]>
2019-08-20 23:01:05 +0900

    ビューの設置

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :groupes, only: [:new, :create, :edit, :update]do
     resources :messages, only: [:index, :create]
+  end
 end
