546c6432c8c9d9610350a76f82895481f692f32d
Tatsuka <[tatsuka.m18@gmail.com]>
2019-08-21 11:59:06 +0900

    ルートの変更とタイプミスの修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,12 +4,12 @@ class GroupsController < ApplicationController
   end
 
   def new
-    @groupe = Groupe.new
-    @groupe.users << current_user
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
-    @groupe = Groupe.new(group_params)
+    @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
