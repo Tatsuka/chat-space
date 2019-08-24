f0144ba6a545850cb9d8aa5c16ec5157a83f28aa
Tatsuka <[tatsuka.m18@gmail.com]>
2019-08-24 16:23:49 +0900

    doneメソッドとHTML追加部分の記述

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,4 +1,26 @@
-$(function(){ 
+$(document).on('turbolinks:load',function(){
+  function buildHTML(message){
+    var content = message.content ? '${message.content}' : "";
+    var img = message.image? '<img src= ${ message.image }>' :"";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="message__detail">
+                    <p class="message__detail__current-user-name">
+                      ${message.user_name}
+                    </p>
+                    <p class="message__detail__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="message_body">
+                    <div>
+                      ${content}
+                    </div>
+                    ${img}
+                  </p>
+                </div>`
+    rerturn html;
+  }
+  
  $('#new_message').on('submit',function(e){
     e.preventDfault();
     var formdata = new FormData(this);
@@ -11,6 +33,12 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false,
+    })
+
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val('');
     })
   })
 });
