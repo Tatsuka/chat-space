87316b5371d276edac1d69ec267c993584dcee98
Tatsuka <[tatsuka.m18@gmail.com]>
2019-08-26 14:56:06 +0900

    選択されたメンバーの移動と削除

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -9,6 +9,12 @@ $(function() {
                `
    return html;
   };
+
+  function appendNoUser(user){
+    var html = `<div class='chat-group-user clearfix'>${ user }</div>`
+    search_list.append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
 
@@ -26,10 +32,40 @@ $(function() {
           $(".user-search-result").append(html);
         });
       }
+      else {
+        appendNoUser("一致するユーザはいません");
+      }
+
     })
 
     .fail(function(){
       alert('通信に失敗しました');
     })
   });
+
+
+
+  function clickHTML(user){
+    var userId = user.attr("data-user-id");
+
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+                  <input name='group[user_ids][]' type='hidden' value="${userId}">
+                  <p class='chat-group-user__name'>${user.attr("data-user-name")}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+               </div>`
+    return html;
+  };
+  $(document).on("click",".user-search-add", function() {
+    $input = $(this);
+    var add_user_html = clickHTML($input);
+    $("#search-users").append(add_user_html);
+      $input.parent()[0].remove();
+  });
+  
+  $(document).on("click",".user-search-remove", function() {
+    $input = $(this);
+    $input.parent().remove();
+  });
+
+
 });
